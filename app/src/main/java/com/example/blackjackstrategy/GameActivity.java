259d9a6325e747254b9b1c46a1f28b064202a6e4
SnoopyH79 <john250032@gmail.com>
2023-07-01 01:22:10 -0700

    Player's second card and Dealers card

--- app/src/main/java/com/example/blackjackstrategy/GameActivity.java
+++ app/src/main/java/com/example/blackjackstrategy/GameActivity.java
@@ -3,69 +3,182 @@ package com.example.blackjackstrategy;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
-    private Button selectedButton;
-    private List<Button> cardButtons;
+    private List<Button> firstCardButtons;
+    private List<Button> secondCardButtons;
+    private List<Button> dealerCardButtons;
 
     private int playerValue;
+    private int dealerValue;
     private TextView playerValueTextView;
-    private int[] cardButtonIds = {
-            R.id.button_A,
-            R.id.button_2,
-            R.id.button_3,
-            R.id.button_4,
-            R.id.button_5,
-            R.id.button_6,
-            R.id.button_7,
-            R.id.button_8,
-            R.id.button_9,
-            R.id.button_10,
-            R.id.button_J,
-            R.id.button_Q,
-            R.id.button_K
+    private TextView dealerValueTextView;
+    private final int[] firstCardButtonIds = {
+            R.id.btn_First_A,
+            R.id.btn_First_2,
+            R.id.btn_First_3,
+            R.id.btn_First_4,
+            R.id.btn_First_5,
+            R.id.btn_First_6,
+            R.id.btn_First_7,
+            R.id.btn_First_8,
+            R.id.btn_First_9,
+            R.id.btn_First_10,
+            R.id.btn_First_J,
+            R.id.btn_First_Q,
+            R.id.btn_First_K
+    };
+
+    private final int[] secondCardButtonIds = {
+            R.id.btn_Second_A,
+            R.id.btn_Second_2,
+            R.id.btn_Second_3,
+            R.id.btn_Second_4,
+            R.id.btn_Second_5,
+            R.id.btn_Second_6,
+            R.id.btn_Second_7,
+            R.id.btn_Second_8,
+            R.id.btn_Second_9,
+            R.id.btn_Second_10,
+            R.id.btn_Second_J,
+            R.id.btn_Second_Q,
+            R.id.btn_Second_K
+    };
+
+    private final int[] dealerCardButtonIds = {
+            R.id.btn_Dealer_A,
+            R.id.btn_Dealer_2,
+            R.id.btn_Dealer_3,
+            R.id.btn_Dealer_4,
+            R.id.btn_Dealer_5,
+            R.id.btn_Dealer_6,
+            R.id.btn_Dealer_7,
+            R.id.btn_Dealer_8,
+            R.id.btn_Dealer_9,
+            R.id.btn_Dealer_10,
+            R.id.btn_Dealer_J,
+            R.id.btn_Dealer_Q,
+            R.id.btn_Dealer_K
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        playerValue = 0;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        cardButtons = new ArrayList<>();
-        for (int id : cardButtonIds) {
+        firstCardButtons = new ArrayList<>();
+        for (int id : firstCardButtonIds) {
             Button button = findViewById(id);
-            cardButtons.add(button);
-            int index = cardButtons.size() - 1;
-            button.setOnClickListener(v -> handleCardButtonClick(cardButtons.get(index), index + 1));
+            firstCardButtons.add(button);
+            button.setOnClickListener(v -> handleCardButtonClick(button, firstCardButtons));
         }
+
+        secondCardButtons = new ArrayList<>();
+        for (int id : secondCardButtonIds) {
+            Button button = findViewById(id);
+            secondCardButtons.add(button);
+            button.setOnClickListener(v -> handleCardButtonClick(button, secondCardButtons));
+        }
+
+        dealerCardButtons = new ArrayList<>();
+        for (int id : dealerCardButtonIds) {
+            Button button = findViewById(id);
+            dealerCardButtons.add(button);
+            button.setOnClickListener(v -> handleDealerCardButtonClick(button, dealerCardButtons));
+        }
+
         playerValueTextView = findViewById(R.id.textView_playerValue);
+        playerValue = 0;
+        dealerValueTextView = findViewById(R.id.textView_dealerValue);
+        dealerValue = 0;
+        updatePlayerValue();
     }
 
-    private void handleCardButtonClick(Button button, int cardValue) {
-        if (selectedButton != null && selectedButton != button) {
-            selectedButton.setSelected(false);
-            playerValue = 0;
-        }
+    private void handleCardButtonClick(Button button, List<Button> cardButtons) {
+        int cardValue = getCardValue(button.getId(), cardButtons);
 
-        if(button.isSelected())
-        {
+        if (button.isSelected()) {
+            button.setSelected(false);
             playerValue -= cardValue;
-        }
-        else
-        {
+        } else {
+            button.setSelected(true);
             playerValue += cardValue;
+            for (Button cardButton : cardButtons) {
+                if (cardButton != button && cardButton.isSelected()) {
+                    cardButton.setSelected(false);
+                    playerValue -= getCardValue(cardButton.getId(), cardButtons);
+                }
+            }
         }
 
-        button.setSelected(!button.isSelected());
-        selectedButton = button;
+        updatePlayerValue();
+    }
 
-        playerValueTextView.setText("Player Value: " + playerValue);
+    private void handleDealerCardButtonClick(Button button, List<Button> cardButtons) {
+        int cardValue = getCardValue(button.getId(), cardButtons);
+
+        if (button.isSelected()) {
+            button.setSelected(false);
+            dealerValue -= cardValue;
+        } else {
+            button.setSelected(true);
+            dealerValue += cardValue;
+            for (Button cardButton : cardButtons) {
+                if (cardButton != button && cardButton.isSelected()) {
+                    cardButton.setSelected(false);
+                    dealerValue -= getCardValue(cardButton.getId(), cardButtons);
+                }
+            }
+        }
+
+        updateDealerValue();
+    }
+
+    private int getCardValue(int buttonId, List<Button> cardButtons) {
+        Button button = findViewById(buttonId);
+        String buttonText = button.getText().toString();
+
+        if (buttonText.equals("J") || buttonText.equals("Q") || buttonText.equals("K")) {
+            return 10;
+        } else {
+            return cardButtons.indexOf(button) + 1;
+        }
+    }
+
+    private void updatePlayerValue() {
+        String valueText;
+        if (playerValue <= 11 && hasAce()) {
+            valueText = "Player Value: " + playerValue + " or " + (playerValue + 10);
+        } else {
+            valueText = "Player Value: " + playerValue;
+        }
+        playerValueTextView.setText(valueText);
+    }
+
+    private void updateDealerValue() {
+        String valueText;
+        if (dealerValue == 1) {
+            valueText = "Dealer Value: " + dealerValue + " or " + (dealerValue + 10);
+        } else {
+            valueText = "Dealer Value: " + dealerValue;
+        }
+        dealerValueTextView.setText(valueText);
+    }
+
+    private boolean hasAce() {
+        for (Button button : firstCardButtons) {
+            if (button.isSelected() && button.getId() == R.id.btn_First_A) {
+                return true;
+            }
+        }
+        for (Button button : secondCardButtons) {
+            if (button.isSelected() && button.getId() == R.id.btn_Second_A) {
+                return true;
+            }
+        }
+        return false;
     }
 }
